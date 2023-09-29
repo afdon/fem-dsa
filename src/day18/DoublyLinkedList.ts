3310a789d248516cc6197b10094b063758c253fd
afdon <53676753+afdon@users.noreply.github.com>
2023-09-29 09:59:21 -0700

    doubly wip

--- src/day18/DoublyLinkedList.ts
+++ src/day18/DoublyLinkedList.ts
@@ -1,13 +1,26 @@
+
+class Node<T> {
+    public data: T;
+    public next?: Node<T>;
+    public prev?: Node<T>;
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    public head: Node<T>;
+    public tail?: Node<T>;
 
     constructor() {
     }
 
     prepend(item: T): void {
-
+        let newNode = new Node<T>
+        newNode.data = item
+        if (!this.head) this.head = newNode
+        let oldHead = this.head
+        this.head = newNode
+        newNode.next = oldHead
+        oldHead.prev = newNode
 }
     insertAt(item: T, idx: number): void {
 
