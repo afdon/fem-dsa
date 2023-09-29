1d43d8114321f233e0b73a4a7c37e5b952b8fb09
afdon <53676753+afdon@users.noreply.github.com>
2023-09-29 09:03:13 -0700

    singly wip

--- src/day18/SinglyLinkedList.ts
+++ src/day18/SinglyLinkedList.ts
@@ -49,19 +49,27 @@ export default class SinglyLinkedList<T> {
         let current = this.head
         if (current.data === item && current.next) { 
             this.head = current.next 
-            return removed
+            return removed 
         }
-        while (current.next && current.next.data === item) {
+        let i = 0
+        while (current.next && current.next.data !== item) {
+            if (i > this.length - 1) return undefined
             current.next = current.next.next
+            i++
         }
-        
+        // now, current.next is the item to remove
+        if (current.next) current.next = current.next.next
+        return removed
 }
     get(idx: number): T | undefined {
         if (!this.head) return undefined
         if (idx > this.length - 1) return undefined
         let current = this.head
-        while (this.head)
-        // 
+        for (let i = 0; i < idx; i++) {
+            if (current.next) current = current.next
+        }
+        // now, current.next is the node at idx
+        if (current.next) return current.next.data // add if condition to appease TS
 }
     removeAt(idx: number): T | undefined {
 
